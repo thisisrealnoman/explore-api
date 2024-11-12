0358061bda4f70076147d5a4c107146f62fd3a7a
thisisrealnoman <nomanontheroad@gmail.com>
2024-11-12 23:20:49 +0600

    api, json

---
+++ api.js
@@ -0,0 +1,2 @@
+const user = {id:1, name: "Pixel F", job: "mobile"};
+console.log(user);
---
+++ json.js
@@ -0,0 +1,5 @@
+const user = {id:1, name: "Pixel F", job: "mobile"};
+// JavaScript Object Notation (JSON)
+const stringified = JSON.stringify(user);
+console.log(user);
+console.log(stringified);
