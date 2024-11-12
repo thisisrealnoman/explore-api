9fd34f6464a4b8e0dc153b88c717429bc6e58445
thisisrealnoman <nomanontheroad@gmail.com>
2024-11-13 03:05:59 +0600

    JSON stringify, parse

--- json.js
+++ json.js
@@ -3,3 +3,27 @@ const user = {id:1, name: "Pixel F", job: "mobile"};
 const stringified = JSON.stringify(user);
 console.log(user);
 console.log(stringified);
+
+/*
+{ id: 1, name: 'Pixel F', job: 'mobile' }
+{"id":1,"name":"Pixel F","job":"mobile"}
+*/
+
+const shop = {
+    owner : 'Sahrukh',
+    address: {
+        street: '123 road',
+        city: "Dhaka",
+        country: "BD"
+    },
+    products: ['laptop', 'mic', 'monitor', 'keyboard'],
+    revenue: 45000,
+    isOpen: true,
+    isNew: false
+}
+
+// console.log(shop);
+const shopJSON = JSON.stringify(shop);
+// console.log(shopJSON);
+const shopObj = JSON.parse(shopJSON);
+console.log(shopObj);
